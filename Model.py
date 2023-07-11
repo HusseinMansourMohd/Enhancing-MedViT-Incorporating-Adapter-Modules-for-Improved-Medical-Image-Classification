c42ce44bab7868e2b35b8380d2f57e65bf5f20f4
HusseinMansourMohd <husseinmcwii13@gmail.com>
2023-07-11 00:51:47 -0500

    LocalityFeedForward

--- Model.py
+++ Model.py
@@ -122,4 +122,92 @@ class ECALayer(nn.Module):
         return x * y.expand_as(x)
     
 
+class SELayer(nn.Module):
+    def __init__(self, channel, reduction=4):
+        super(SELayer, self).__init__()
+        self.avg_pool = nn.AdaptivePool2d(1)
+        self.fc = nn.Sequential(
+            nn.linear(channel, channel // reduction)
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // reduction , channel)
+            h_sigmoid()
+        )
+    def forward(self, x):
+        b, c _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b,c,1,1)
+        return x*y
+    
+
+class LocalityFeedForward(nn.Module):
+    def __init__(self, in_dim , out_dim, stride, expand_ratio=4. , act='hs+se', reduction=4,
+                 wp_dp_conv=False, dp_first = False):
+         """
+        :param in_dim: the input dimension
+        :param out_dim: the output dimension. The input and output dimension should be the same.
+        :param stride: stride of the depth-wise convolution.
+        :param expand_ratio: expansion ratio of the hidden dimension.
+        :param act: the activation function.
+                    relu: ReLU
+                    hs: h_swish
+                    hs+se: h_swish and SE module
+                    hs+eca: h_swish and ECA module
+                    hs+ecah: h_swish and ECA module. Compared with eca, h_sigmoid is used.
+        :param reduction: reduction rate in SE module.
+        :param wo_dp_conv: without depth-wise convolution.
+        :param dp_first: place depth-wise convolution as the first layer.
+        """
+         super(LocalityFeedForward, self).__init__()
+         hidden_dim = int(in_dim * expand_ratio)
+         kernel_size = 3
+
+         layers = []
+         # the first linear layer is replaced by 1x1 convolution.
+         layers.extend([
+            nn.Conv2d(in_dim, hidden_dim, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(hidden_dim),
+            h_swish() if act.find('hs') >= 0 else nn.ReLU6(inplace=True)])
+         
+         # the depth-wise conveloution between the two linear layers
+         if not wo_dp_conv:
+             dp =[
+                 nn.Conv2d(hidden_dim, hidden_dim, kernel_size, stride, kernel_size// 2 , groups=hidden_dim, bais=False),
+                 nn.BatchNorm2d(hidden_dim),
+                 h_swish() if act.find('hs') >= 0 else nn.RelU6(inplace=True)
+             ]
+             if pd_first:
+                 layers = dp + layers
+             else:
+                 layers.extend(dp)
+
+
+         if act.find('+') >= 0:
+            attn = act.split('+')[1]
+            if attn == 'se':
+                layers.append(SELayer(hidden_dim, reduction=reduction))
+            elif attn.find('eca') >= 0:
+                 layers.append(ECALayer(hidden_dim, sigmoid=attn == 'eca'))
+            else:
+                raise NotImplementedError('Activition type { is not implement}'.format(act))
+            
+            #the second linear layer is repaced by 1x1 convloution
+            layers.extend(
+                [
+                    nn.Conv2d(hidden_dim, out_dim , 1, 1, 0, bais=False),
+                    nn.BatchNorm2d(out_dim)
+                ]
+            )
+            self.conv = nn.Sequential(*layers)
+
+    def forward(self, x):
+        x =x + self.conv(x)
+        return x
+    
+
+             
         
+
+
+
+
+         
