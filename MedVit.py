286188282acdcc7c3d07d2a9636b89abbc460be1
HusseinMansourMohd <husseinmcwii13@gmail.com>
2023-07-17 18:46:03 -0500

    medvit_adapter_class

--- MedVit.py
+++ MedVit.py
@@ -11,6 +11,7 @@ import torch.utils.checkpoint as checkpoint
 from einops import rearrange
 from timm.models.layers import DropPath, trunc_normal_
 from timm.models.registry import register_model
+from ops.modules import MSDeformAttn
 from torch import nn
 from utils import merge_pre_bn
 
@@ -538,11 +539,185 @@ class MedViT(nn.Module):
         x = self.proj_head(x)
         return x
 
+from .adapter_modules import SpatialPriorModule, InteractionBlock, deform_inputs
 
 
 
 class MedVit_adapter(nn.Module):
+    def __init__(self, stem_chs, depths, path_dropout, attn_drop=0, drop=0, num_classes=1000,
+                 strides=[1, 2, 2, 2], sr_ratios=[8, 4, 2, 1], head_dim=32, mix_block_ratio=0.75,
+                 use_checkpoint=False, pretrain_size=224, num_heads=12, conv_inplane=64, n_points=4,
+                 deform_num_heads=6, init_values=0., interaction_indexes=None, with_cffn=True,
+                 cffn_ratio=0.25, deform_ratio=1.0, add_vit_feature=True, pretrained=None,
+                 use_extra_extractor=True, with_cp=False, *args, **kwargs):
+        super(MedVit_adapter, self).__init__(num_heads=num_heads , pretrained=pretrained)
+        self.use_checkpoint = use_checkpoint
+
+        self.stage_out_channels = [[96] * (depths[0]),
+                                   [192] * (depths[1] - 1) + [256],
+                                   [384, 384, 384, 384, 512] * (depths[2] // 5),
+                                   [768] * (depths[3] - 1) + [1024]]
+
+        # Next Hybrid Strategy
+        self.stage_block_types = [[ECB] * depths[0],
+                                  [ECB] * (depths[1] - 1) + [LTB],
+                                  [ECB, ECB, ECB, ECB, LTB] * (depths[2] // 5),
+                                  [ECB] * (depths[3] - 1) + [LTB]]
+
+        self.stem = nn.Sequential(
+            ConvBNReLU(3, stem_chs[0], kernel_size=3, stride=2),
+            ConvBNReLU(stem_chs[0], stem_chs[1], kernel_size=3, stride=1),
+            ConvBNReLU(stem_chs[1], stem_chs[2], kernel_size=3, stride=1),
+            ConvBNReLU(stem_chs[2], stem_chs[2], kernel_size=3, stride=2),
+        )
+        input_channel = stem_chs[-1]
+        features = []
+        idx = 0
+        dpr = [x.item() for x in torch.linspace(0, path_dropout, sum(depths))]  # stochastic depth decay rule
+        for stage_id in range(len(depths)):
+            numrepeat = depths[stage_id]
+            output_channels = self.stage_out_channels[stage_id]
+            block_types = self.stage_block_types[stage_id]
+            for block_id in range(numrepeat):
+                if strides[stage_id] == 2 and block_id == 0:
+                    stride = 2
+                else:
+                    stride = 1
+                output_channel = output_channels[block_id]
+                block_type = block_types[block_id]
+                if block_type is ECB:
+                    layer = ECB(input_channel, output_channel, stride=stride, path_dropout=dpr[idx + block_id],
+                                drop=drop, head_dim=head_dim)
+                    features.append(layer)
+                elif block_type is LTB:
+                    layer = LTB(input_channel, output_channel, path_dropout=dpr[idx + block_id], stride=stride,
+                                sr_ratio=sr_ratios[stage_id], head_dim=head_dim, mix_block_ratio=mix_block_ratio,
+                                attn_drop=attn_drop, drop=drop)
+                    features.append(layer)
+                input_channel = output_channel
+            idx += numrepeat
+        self.features = nn.Sequential(*features)
+
+        self.norm = nn.BatchNorm2d(output_channel, eps=NORM_EPS)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.proj_head = nn.Sequential(
+            nn.Linear(output_channel, num_classes),
+        )
+
+        self.stage_out_idx = [sum(depths[:idx + 1]) - 1 for idx in range(len(depths))]
+        print('initialize_weights...')
+        self._initialize_weights()
+
+    def merge_bn(self):
+        self.eval()
+        for idx, module in self.named_modules():
+            if isinstance(module, ECB) or isinstance(module, LTB):
+                module.merge_bn()
+
+    def _initialize_weights(self):
+        for n, m in self.named_modules():
+            if isinstance(m, (nn.BatchNorm2d, nn.GroupNorm, nn.LayerNorm, nn.BatchNorm1d)):
+                nn.init.constant_(m.weight, 1.0)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                trunc_normal_(m.weight, std=.02)
+                if hasattr(m, 'bias') and m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Conv2d):
+                trunc_normal_(m.weight, std=.02)
+                if hasattr(m, 'bias') and m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm) or isinstance(m, nn.BatchNorm2d):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+        elif isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+            fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+            fan_out //= m.groups
+            m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
+            if m.bias is not None:
+                m.bias.data.zero_()
     
+    def _get_pos_embed(self, pos_embed, H, W):
+        pos_embed = pos_embed.reshape(
+            1, self.pretrain_size[0] // 16, self.pretrain_size[1] // 16, -1).permute(0, 3, 1, 2)
+        pos_embed = F.interpolate(pos_embed, size=(H, W), mode='bicubic', align_corners=False).\
+            reshape(1, -1, H * W).permute(0, 2, 1)
+        return pos_embed
+
+    def _init_deform_weights(self, m):
+        if isinstance(m, MSDeformAttn):
+            m._reset_parameters()
+
+    def _add_level_embed(self, c2, c3, c4):
+        c2 = c2 + self.level_embed[0]
+        c3 = c3 + self.level_embed[1]
+        c4 = c4 + self.level_embed[2]
+        return c2, c3, c4
+
+    def forward(self, x):
+        deform_inputs1 , deform_inputs2 = deform_inputs(x)
+
+        # SRM forward
+        c1, c2, c3, c4 = self.spm(x)
+        c2, c3, c4 = self._add_level_embed(c2, c3, c4)
+        c = torch.cat([c2, c3, c4], dim=1)
+
+        #Patch Embedding forward
+        x, H, W = self.patch_embed(x)
+        bs, n, dim = x.shape
+        pos_embed = self._get_pos_embed(self.pos_embed[:,1:], H, W)
+        x = self.pos_drop(x + pos_embed)
+
+        # Iteration
+        outs = list()
+        for i, layer in enumerate(self.interactions):
+            indexes = self.interaction_indexes[i]
+            x,c = layer(x, c, self.blocks[indexes[0]:indexes[-1] + 1],
+                        deform_inputs1, deform_inputs2, H, W)
+            outs.append(x.transpose(1,2).veiw(bs, dim, H, W).contiguous)
+
+        # Split & Reshape
+        c2 = c[:, 0:c2.size(1),:]
+        c3 = c[:, c2.size(1):c2.size(1) + c3.size(1), :]
+        c4 = c[:, c2.size(1) + c3.size(1), :]
+
+        c2 = c2.transpose(1,2).veiw(bs, dim, H * 2, W *2).contiguous()
+        c3 = c3.transpose(1,2).veiw(bs, dim, H, W).contiguous()
+        c4 = c4.transpose(1,2).view(bs, dim, H//2, W//2).contiguous()
+
+        if self.add_vit_feature:
+            x1, x2, x3, x4 = outs
+            x1 = F.interpolate(x1, scale_factor=4, mode='bilinear', align_corners=False)
+            x2 = F.interpolate(x2, scale_factor=2, mode='bilinear', align_corners=False)
+            x4 = F.interpolate(x4, scale_factor=0.5, mode='bilinear', align_corners=False)
+            c1, c2, c3, c4 = c1 + x1, c2 + x2, c3 + x3, c4 + x4
+
+        # Final Norm
+        f1 = self.norm(c1)
+        f2 = self.norm(c2)
+        f3 = self.norm(c3)
+        f4 = self.norm(c4)
+
+        return [f1, f2, f3, f4]
+        # x = self.stem(x)
+        # for idx, layer in enumerate(self.features):
+        #     if self.use_checkpoint:
+        #         x = checkpoint.checkpoint(layer, x)
+        #     else:
+        #         x = layer(x)
+        # x = self.norm(x)
+        # x = self.avgpool(x)
+        # x = torch.flatten(x, 1)
+        # x = self.proj_head(x)
+        # return x
+
     
 @register_model
 def MedViT_small(pretrained=False, pretrained_cfg=None, **kwargs):
