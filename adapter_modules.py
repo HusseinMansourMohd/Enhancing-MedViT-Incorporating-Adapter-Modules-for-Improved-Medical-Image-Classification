a7b42ffd33b70eda771fd76671705b287480b0eb
HusseinMansourMohd <husseinmcwii13@gmail.com>
2023-07-24 00:21:07 -0500

    adapter imports

--- adapter_modules.py
+++ adapter_modules.py
@@ -3,7 +3,7 @@ from functools import partial
 
 import torch
 import torch.nn as nn
-from DeformableDETR.models.ops.modules import ms_deform_attn 
+from models.ops import ms_deform_attn 
 from timm.models.layers import DropPath
 import torch.utils.checkpoint as cp
 
