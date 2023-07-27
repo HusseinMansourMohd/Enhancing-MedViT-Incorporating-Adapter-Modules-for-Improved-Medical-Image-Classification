65df6e379999e1a9a74dc8a997b6f018b7581c5a
HusseinMansourMohd <husseinmcwii13@gmail.com>
2023-07-26 23:10:45 -0500

    output_channel  input_channel

--- MedVit_Adapter.py
+++ MedVit_Adapter.py
@@ -20,6 +20,9 @@ class MedVit_adapter(nn.Module):
         self.embed_dim = embed_dim
         self.stem_chs = stem_chs
         input_channel = stem_chs[-1]
+        output_channel = 512
+        self.input_channel = input_channel
+        self.output_channel = output_channel
         self.patch_embed = PatchEmbed(input_channel, out_channel, stride)
 
         self._initialize_hyperparameters(path_dropout, use_checkpoint, pretrain_size, interaction_indexes, 
