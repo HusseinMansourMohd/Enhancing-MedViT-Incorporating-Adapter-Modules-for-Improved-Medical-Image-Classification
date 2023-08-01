6d9355aa2f352ac174e5c832ba50c47bcab5295f
HusseinMansourMohd <husseinmcwii13@gmail.com>
2023-07-31 21:24:12 -0500

    reshape

--- MedVit_Adapter.py
+++ MedVit_Adapter.py
@@ -215,7 +215,9 @@ class MedVit_adapter(nn.Module):
         c4 = c[:, c2.size(1) + c3.size(1), :]
         c2 = c2.transpose(1,2).reshape(bs, dim, H * 2, W *2).contiguous()
         c3 = c3.transpose(1,2).reshape(bs, dim, H, W).contiguous()
-        c4 = c4.transpose(1,2).reshape(bs, 1, 1, W//2).contiguous()
+        c4 = c4.transpose(1,2)
+        c4 = c4.reshape(bs, c4.shape[1]//2, 2, c4.shape[2]).contiguous()
+
 
         # Feature interpolation and addition
         if self.add_vit_feature:
