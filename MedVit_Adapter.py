658ac3386e64ac653fa683a8c86017a4839a77c2
HusseinMansourMohd <husseinmcwii13@gmail.com>
2023-07-31 21:28:47 -0500

    Rshaping

--- MedVit_Adapter.py
+++ MedVit_Adapter.py
@@ -215,6 +215,8 @@ class MedVit_adapter(nn.Module):
         c4 = c[:, c2.size(1) + c3.size(1), :]
         c2 = c2.transpose(1,2).reshape(bs, dim, H * 2, W *2).contiguous()
         c3 = c3.transpose(1,2).reshape(bs, dim, H, W).contiguous()
+        if len(c4.shape) < 3:
+                c4 = c4.unsqueeze(2)
         c4 = c4.transpose(1,2)
         c4 = c4.reshape(bs, c4.shape[1]//2, 2, c4.shape[2]).contiguous()
 
