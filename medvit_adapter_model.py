a3b526c0fb28ca6f2d637fa3b51c0829ba2df01f
HusseinMansourMohd <husseinmcwii13@gmail.com>
2023-08-02 21:03:22 -0500

    medvit adapter model

--- medvit_adapter_model.py
+++ medvit_adapter_model.py
@@ -21,7 +21,10 @@ NORM_EPS = 1e-5
 class MedViT(nn.Module):
     def __init__(self, stem_chs, depths, path_dropout, attn_drop=0, drop=0, num_classes=1000,
                  strides=[1, 2, 2, 2], sr_ratios=[8, 4, 2, 1], head_dim=32, mix_block_ratio=0.75,
-                 use_checkpoint=False):
+                 use_checkpoint=False,  pretrain_size=224, num_heads=12, conv_inplane=64, n_points=4,
+                 deform_num_heads=6, init_values=0., interaction_indexes=None, with_cffn=True,
+                 cffn_ratio=0.25, deform_ratio=1.0, add_vit_feature=True, pretrained=None,
+                 use_extra_extractor=True, with_cp=False,):
         super(MedViT, self).__init__()
         self.use_checkpoint = use_checkpoint
 
@@ -42,6 +45,51 @@ class MedViT(nn.Module):
             ConvBNReLU(stem_chs[1], stem_chs[2], kernel_size=3, stride=1),
             ConvBNReLU(stem_chs[2], stem_chs[2], kernel_size=3, stride=2),
         )
+        self.interaction_indexes = interaction_indexes
+        self.add_vit_feature = add_vit_feature
+        embed_dim = self.embed_dim
+        
+        self.level_embed = nn.Parameter(torch.zeros(3, embed_dim))
+        self.spm = SpatialPriorModule(inplanes=conv_inplane, embed_dim=embed_dim, with_cp=False)
+        self.interactions = nn.Sequential(*[
+            InteractionBlock(dim=embed_dim, num_heads=deform_num_heads, n_points=n_points,
+                             init_values=init_values, drop_path=self.drop_path_rate,
+                             norm_layer=self.norm_layer, with_cffn=with_cffn,
+                             cffn_ratio=cffn_ratio, deform_ratio=deform_ratio,
+                             extra_extractor=((True if i == len(interaction_indexes) - 1
+                                               else False) and use_extra_extractor),
+                             with_cp=with_cp)
+            for i in range(len(interaction_indexes))
+        ])
+        self.up = nn.ConvTranspose2d(embed_dim, embed_dim, 2, 2)
+        self.norm1 = nn.SyncBatchNorm(embed_dim)
+        self.norm2 = nn.SyncBatchNorm(embed_dim)
+        self.norm3 = nn.SyncBatchNorm(embed_dim)
+        self.norm4 = nn.SyncBatchNorm(embed_dim)
+
+        self.up.apply(self._init_weights)
+        self.spm.apply(self._init_weights)
+        self.interactions.apply(self._init_weights)
+        self.apply(self._init_deform_weights)
+        normal_(self.level_embed)
+
+        def _init_weights(self, m):
+            if isinstance(m, nn.Linear):
+                trunc_normal_(m.weight, std=.02)
+                if isinstance(m, nn.Linear) and m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.LayerNorm) or isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.bias, 0)
+                nn.init.constant_(m.weight, 1.0)
+            elif isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+                fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                fan_out //= m.groups
+                m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+
+        
+
         input_channel = stem_chs[-1]
         features = []
         idx = 0
@@ -102,6 +150,7 @@ class MedViT(nn.Module):
                     nn.init.constant_(m.bias, 0)
 
     def forward(self, x):
+        #medVit Network
         x = self.stem(x)
         for idx, layer in enumerate(self.features):
             if self.use_checkpoint:
@@ -110,6 +159,70 @@ class MedViT(nn.Module):
                 x = layer(x)
         x = self.norm(x)
         x = self.avgpool(x)
-        x = torch.flatten(x, 1)
-        x = self.proj_head(x)
-        return x
+        x_flattened = torch.flatten(x, 1)
+        #x = self.proj_head(x)
+        #adapter Network
+        deform_inputs1, deform_inputs2 = deform_inputs(x)
+        
+        # SPM forward
+        c1, c2, c3, c4 = self.spm(x)
+        c2, c3, c4 = self._add_level_embed(c2, c3, c4)
+        c = torch.cat([c2, c3, c4], dim=1)
+
+        # Patch Embedding forward
+        x, H, W = self.patch_embed(x)
+        bs, n, dim = x.shape
+        pos_embed = self._get_pos_embed(self.pos_embed[:, 1:], H, W)
+        x = self.pos_drop(x + pos_embed)
+
+        # Interaction
+        outs = list()
+        for i, layer in enumerate(self.interactions):
+            indexes = self.interaction_indexes[i]
+            x, c = layer(x, c, self.blocks[indexes[0]:indexes[-1] + 1],
+                         deform_inputs1, deform_inputs2, H, W)
+            outs.append(x.transpose(1, 2).view(bs, dim, H, W).contiguous())
+        
+        # Split & Reshape
+        c2 = c[:, 0:c2.size(1), :]
+        c3 = c[:, c2.size(1):c2.size(1) + c3.size(1), :]
+        c4 = c[:, c2.size(1) + c3.size(1):, :]
+
+        c2 = c2.transpose(1, 2).view(bs, dim, H * 2, W * 2).contiguous()
+        c3 = c3.transpose(1, 2).view(bs, dim, H, W).contiguous()
+        c4 = c4.transpose(1, 2).view(bs, dim, H // 2, W // 2).contiguous()
+        c1 = self.up(c2) + c1
+
+        if self.add_vit_feature:
+            x1, x2, x3, x4 = outs
+            x1 = F.interpolate(x1, scale_factor=4, mode='bilinear', align_corners=False)
+            x2 = F.interpolate(x2, scale_factor=2, mode='bilinear', align_corners=False)
+            x4 = F.interpolate(x4, scale_factor=0.5, mode='bilinear', align_corners=False)
+            c1, c2, c3, c4 = c1 + x1, c2 + x2, c3 + x3, c4 + x4
+
+        # Final Norm
+        f1 = self.norm1(c1)
+        f2 = self.norm2(c2)
+        f3 = self.norm3(c3)
+        f4 = self.norm4(c4)
+        
+        # apply global average pooling to each feature map
+        f1_pooled = F.adaptive_avg_pool2d(f1, (1, 1))
+        f2_pooled = F.adaptive_avg_pool2d(f2, (1, 1))
+        f3_pooled = F.adaptive_avg_pool2d(f3, (1, 1))
+        f4_pooled = F.adaptive_avg_pool2d(f4, (1, 1))
+
+        # flatten feature vectors
+        f1_pooled = torch.flatten(f1_pooled, 1)
+        f2_pooled = torch.flatten(f2_pooled, 1)
+        f3_pooled = torch.flatten(f3_pooled, 1)
+        f4_pooled = torch.flatten(f4_pooled, 1)
+
+        # concatenate feature vectors
+        features = torch.cat([f1_pooled, f2_pooled, f3_pooled, f4_pooled,x_flattened], dim=1)
+
+        # pass through a fully-connected layer to generate class predictions
+        out = self.fc(features)
+
+        return out
+        #return x
