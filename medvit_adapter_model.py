6b8d8ac2573aa9fd36c2e8d71118fd446b49e3de
HusseinMansourMohd <husseinmcwii13@gmail.com>
2023-08-02 19:35:03 -0500

    medvit_adapter_model

---
+++ medvit_adapter_model.py
@@ -0,0 +1,115 @@
+"""
+Author: Hussein Mansour Mohamed Mansour
+Email: hussein.mansour@tamu.edu
+
+Enhancing MedViT: Incorporating Adapter Modules for Improved Medical Image Datasets.
+"""
+from functools import partial
+import math
+import torch
+import torch.utils.checkpoint as checkpoint
+from einops import rearrange
+from timm.models.layers import DropPath, trunc_normal_
+from timm.models.registry import register_model
+# from ops.modules import MSDeformAttn
+from torch import nn
+from utils import merge_pre_bn
+from torch.nn.init import normal_
+
+NORM_EPS = 1e-5
+
+class MedViT(nn.Module):
+    def __init__(self, stem_chs, depths, path_dropout, attn_drop=0, drop=0, num_classes=1000,
+                 strides=[1, 2, 2, 2], sr_ratios=[8, 4, 2, 1], head_dim=32, mix_block_ratio=0.75,
+                 use_checkpoint=False):
+        super(MedViT, self).__init__()
+        self.use_checkpoint = use_checkpoint
+
+        self.stage_out_channels = [[96] * (depths[0]),
+                                   [192] * (depths[1] - 1) + [256],
+                                   [384, 384, 384, 384, 512] * (depths[2] // 5),
+                                   [768] * (depths[3] - 1) + [1024]]
+
+        # Next Hybrid Strategy
+        self.stage_block_types = [[ECB] * depths[0],
+                                  [ECB] * (depths[1] - 1) + [LTB],
+                                  [ECB, ECB, ECB, ECB, LTB] * (depths[2] // 5),
+                                  [ECB] * (depths[3] - 1) + [LTB]]
+
+        self.stem = nn.Sequential(
+            ConvBNReLU(3, stem_chs[0], kernel_size=3, stride=2),
+            ConvBNReLU(stem_chs[0], stem_chs[1], kernel_size=3, stride=1),
+            ConvBNReLU(stem_chs[1], stem_chs[2], kernel_size=3, stride=1),
+            ConvBNReLU(stem_chs[2], stem_chs[2], kernel_size=3, stride=2),
+        )
+        input_channel = stem_chs[-1]
+        features = []
+        idx = 0
+        dpr = [x.item() for x in torch.linspace(0, path_dropout, sum(depths))]  # stochastic depth decay rule
+        for stage_id in range(len(depths)):
+            numrepeat = depths[stage_id]
+            output_channels = self.stage_out_channels[stage_id]
+            block_types = self.stage_block_types[stage_id]
+            for block_id in range(numrepeat):
+                if strides[stage_id] == 2 and block_id == 0:
+                    stride = 2
+                else:
+                    stride = 1
+                output_channel = output_channels[block_id]
+                block_type = block_types[block_id]
+                if block_type is ECB:
+                    layer = ECB(input_channel, output_channel, stride=stride, path_dropout=dpr[idx + block_id],
+                                drop=drop, head_dim=head_dim)
+                    features.append(layer)
+                elif block_type is LTB:
+                    layer = LTB(input_channel, output_channel, path_dropout=dpr[idx + block_id], stride=stride,
+                                sr_ratio=sr_ratios[stage_id], head_dim=head_dim, mix_block_ratio=mix_block_ratio,
+                                attn_drop=attn_drop, drop=drop)
+                    features.append(layer)
+                input_channel = output_channel
+            idx += numrepeat
+        self.features = nn.Sequential(*features)
+
+        self.norm = nn.BatchNorm2d(output_channel, eps=NORM_EPS)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.proj_head = nn.Sequential(
+            nn.Linear(output_channel, num_classes),
+        )
+
+        self.stage_out_idx = [sum(depths[:idx + 1]) - 1 for idx in range(len(depths))]
+        print('initialize_weights...')
+        self._initialize_weights()
+
+    def merge_bn(self):
+        self.eval()
+        for idx, module in self.named_modules():
+            if isinstance(module, ECB) or isinstance(module, LTB):
+                module.merge_bn()
+
+    def _initialize_weights(self):
+        for n, m in self.named_modules():
+            if isinstance(m, (nn.BatchNorm2d, nn.GroupNorm, nn.LayerNorm, nn.BatchNorm1d)):
+                nn.init.constant_(m.weight, 1.0)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                trunc_normal_(m.weight, std=.02)
+                if hasattr(m, 'bias') and m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Conv2d):
+                trunc_normal_(m.weight, std=.02)
+                if hasattr(m, 'bias') and m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        x = self.stem(x)
+        for idx, layer in enumerate(self.features):
+            if self.use_checkpoint:
+                x = checkpoint.checkpoint(layer, x)
+            else:
+                x = layer(x)
+        x = self.norm(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.proj_head(x)
+        return x
